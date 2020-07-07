ee8b6002c2a1ff6ab4bc038feced9f7908486e6f
Alex <failemotions95@gmail.com>
2020-07-07 21:42:40 +0300

    Updates

---
+++ precache-manifest.70c3fb86307cdd5a088af43a42ee9ff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1405a414e863740a2e65e49fd0fe8b2",
+    "url": "/fullstackopen-course-part-6/index.html"
+  },
+  {
+    "revision": "64b363405327c7d296c6",
+    "url": "/fullstackopen-course-part-6/static/js/2.b49a9e53.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fullstackopen-course-part-6/static/js/2.b49a9e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae2f1c395439fbdabe3",
+    "url": "/fullstackopen-course-part-6/static/js/main.9db8e750.chunk.js"
+  },
+  {
+    "revision": "cc922a90c2acfa69afa6",
+    "url": "/fullstackopen-course-part-6/static/js/runtime-main.7830c562.js"
+  }
+]);
